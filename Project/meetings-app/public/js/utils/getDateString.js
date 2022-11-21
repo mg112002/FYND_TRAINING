42787d5165d24f68c7f82202d179341e7eebbe96
MANGALAM GUPTA <82514451+mg112002@users.noreply.github.com>
2022-11-21 18:42:00 +0530

    Delete Project/meetings-app/public directory

--- Project/meetings-app/public/js/utils/getDateString.js
+++
@@ -1,8 +0,0 @@
-export default function getDateString(date) {
-    let dateObj = new Date(date)
-    const dateVal = dateObj.getDate().toString()
-    // const month = (dateObj.getMonth() + 1).toString()
-    const month = dateObj.toLocaleString("en-US", { month: "long" })
-    const year = dateObj.getFullYear().toString()
-    return `${dateVal} ${month} ${year}`
-}
